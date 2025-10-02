7a93666ac1652c0f13ea59dd24354572f62b520f
jalalnx <jalalnx99@gmail.com>
2025-10-02 18:59:11 +0200

    up

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/0x2nac0nda">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="*" element={<NotFound />} />
